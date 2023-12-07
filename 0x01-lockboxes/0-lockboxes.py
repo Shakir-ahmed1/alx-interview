894ed4cfbf8354179cc17b170efc356811a1560d
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-12-08 00:13:11 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """ a module that checks unlockabily"""
+
+
 def canUnlockAll(boxes):
     """ a function that checks if a box is unlockable"""
     n = len(boxes)
-    if n <= 1:
+    if n <= 1 or not boxes:
         return True
     opened_boxes = [0, *boxes[0]]
     # A key with the same number as a box opens that box
     for box in range(n):
         if box in boxes and box not in opened_boxes:
             opened_boxes.append(box)
-    
+
     for box in range(n):
         for b in opened_boxes:
             if b < n:
                 opened_boxes.extend(boxes[b])
                 opened_boxes = list(set(opened_boxes))
-    
+
     correct_keys = n * [0]
     for box in range(n):
         if box in opened_boxes:
             correct_keys[box] = 1
-    
+
     if 0 in correct_keys:
         return False
     else:
         return True
-
-
-
-    # Return True if all boxes can be opened, else return False
