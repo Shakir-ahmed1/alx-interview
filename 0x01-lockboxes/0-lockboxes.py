fdc6575961299a8abf1dfe9f5a325936715c923b
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-12-08 00:21:20 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,16 +13,16 @@ def canUnlockAll(boxes):
             opened_boxes.append(box)
 
     for box in range(n):
+        temp = opened_boxes.copy()
         for b in opened_boxes:
             if b < n:
-                opened_boxes.extend(boxes[b])
-                opened_boxes = sorted(set(opened_boxes))
+                temp.extend(boxes[b])
+        opened_boxes = sorted(set(temp))
 
     correct_keys = n * [0]
     for box in range(n):
         if box in opened_boxes:
             correct_keys[box] = 1
-
     if 0 in correct_keys:
         return False
     else:
