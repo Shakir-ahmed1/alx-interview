a3c33811a82c7a63e68b3fd036118f5626d1eb7a
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-12-08 00:18:22 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,7 +8,6 @@ def canUnlockAll(boxes):
     if n <= 1 or not boxes:
         return True
     opened_boxes = [0, *boxes[0]]
-    # A key with the same number as a box opens that box
     for box in range(n):
         if box in boxes and box not in opened_boxes:
             opened_boxes.append(box)
@@ -17,7 +16,7 @@ def canUnlockAll(boxes):
         for b in opened_boxes:
             if b < n:
                 opened_boxes.extend(boxes[b])
-                opened_boxes = list(set(opened_boxes))
+                opened_boxes = sorted(set(opened_boxes))
 
     correct_keys = n * [0]
     for box in range(n):
