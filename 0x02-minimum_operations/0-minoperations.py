7fe6c7976042ada89660aaca69cf721ec47fd35e
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-14 19:40:40 +0000

    submit code and readme

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a module that computes minimum operations to reach a number
+by only copying and pasting"""
+
+
+def minOperations(n):
+    """ returns minimum operations to reach a number"""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return i + minOperations(n//i)
+        return n
