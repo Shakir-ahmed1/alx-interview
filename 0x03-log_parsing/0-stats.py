e9e0d6f3f7a8ea1b92d56927a44e9b78e376eb23
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-27 19:19:53 +0000

    ignoring status code type

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,14 +12,15 @@ status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                 '404': 0, '405': 0, '500': 0}
 try:
     for line in stdin:
-        valid_line = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
+        valid_line = re.match(r'.*-.*'
                               r'\[.+\] "GET \/projects\/260 HTTP\/1.1"'
-                              r' (\d+) (\d+)', line)
+                              r' (.*) (\d+)', line)
 
         if not valid_line:
             continue
         size_list.append(int(valid_line.group(2)))
-        status_codes[valid_line.group(1)] += 1
+        if valid_line.group(1) in status_codes:
+            status_codes[valid_line.group(1)] += 1
         if counter % 10 == 0:
             total_file_size = sum(size_list)
             print(f"File size: {total_file_size}")
@@ -29,7 +30,7 @@ try:
         counter += 1
 
 
-except Exception:
+except KeyboardInterrupt:
     pass
 finally:
     total_file_size = sum(size_list)
