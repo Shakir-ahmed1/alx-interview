041f5831d5ffe57932dd18d55ff3d46a88683326
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-03 14:52:08 +0000

    submit code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
 """ utf validation module """
-
+def is_btwn(data, bounds):
+    if bounds[0] <= data <= bounds[1]:
+        return True
+    return False
 
 def validUTF8(data):
     """ UTF-8 validator """
+    invalid_numbers = [0xC1, 0xC0, 0xF5, 0XF6, 0XF7, 0XF8, 0XF9, 0XFA,
+                       0XFB, 0XFC, 0XFD, 0XFE, 0XFF]
+    asc = (0x00, 0x7f)
+    cont = (0x80, 0xbf)
+    len2 = (0xc2, 0xdf)
+    len31 = (0xe1, 0x3c)
+    len32 = (0x3e, 0x3f)
+    temp = []
     for d in data:
         if not 256 > d >= 0:
             return False
+        if not temp:
+            if is_btwn(d, asc):
+                continue
+            if is_btwn(d, cont):
+                return False
+            if is_btwn(d, len2) or is_btwn(d, len31) or is_btwn(d, len32):
+                return False
+        else:
+            if is_btwn(d, asc):
+                temp = []
+                continue
+            if is_btwn(d, cont):
+                continue
+            if is_btwn(d, len2) or is_btwn(d, len31) or is_btwn(d, len32):
+                continue
+            if d in invald:
+                return False
     return True
