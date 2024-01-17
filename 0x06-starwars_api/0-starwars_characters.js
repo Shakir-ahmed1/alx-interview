03c9234105dd9f05cd2285d3c5ec7395b1fc52db
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-17 13:52:34 +0000

    semistandard coding style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/'+ process.argv[2], function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(err);
   } else {
-    const links = JSON.parse(body)
-    function chars(i) {
+    const links = JSON.parse(body);
+    function chars (i) {
       if (i < links.characters.length) {
         request(links.characters[i], function (err, resp, bod) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(JSON.parse(bod).name);
-          chars(i + 1);
-        }
-      });
-	
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(JSON.parse(bod).name);
+            chars(i + 1);
+          }
+        });
       }
     }
     chars(0);
   }
 });
+
