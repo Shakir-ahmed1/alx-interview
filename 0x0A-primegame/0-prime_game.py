06a8c0520ef197ecc18cd9fa4e52f19699216323
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-15 11:54:53 +0300

    increasing primes counting effieciency

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,31 +5,31 @@
 def count_primes(n):
     """ generates prime numbers"""
     prime = [True for _ in range(n + 1)]
-    # Mark 0 and 1 as not prime
-    prime[0] = prime[1] = False
-    # Loop from 2 to the square root of n
+    prime[0] = False
+    if len(prime) > 1:
+        prime[1] = False
     for i in range(2, int(n**0.5) + 1):
-        # If i is prime, mark all its multiples as not prime
         if prime[i]:
             for j in range(i * i, n + 1, i):
                 prime[j] = False
-    # Return the number of True values in the array
     return sum(prime)
 
 
 def isWinner(x, nums):
     """ determines who is the winner in the prime game"""
-    maria = 0
-    ben = 0
+    players = ['Maria', 'Ben']
+    results = [0, 0]
+    rounds = 0
     for n in nums:
-        primes = count_primes(n)
-        if primes % 2 == 1:
-            maria += 1
-        else:
-            ben += 1
-    if maria > ben:
-        return "Maria"
-    elif maria < ben:
-        return "Ben"
+        if rounds == x:
+            break
+        rounds += 1
+        index = (count_primes(n) + 1) % 2
+        results[index] += 1
+
+    if results[0] > results[1]:
+        return players[0]
+    elif results[0] < results[1]:
+        return players[1]
     else:
         return None
